fde76d113edf6fd218dd20308f3a5d850bc6448d
Elliot Bouvier <ebouvier@student.42.fr>
2023-10-03 10:52:15 +0200

    apply also on bonus

--- sources/parsing/textures.c
+++ sources/parsing/textures.c
@@ -6,7 +6,7 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 09:42:23 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/10/02 15:27:28 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/10/03 10:47:22 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,22 +28,28 @@ static void	get_textures(char *line, t_orientation orientation,
 		data->we = ft_strtrim(line, "\n ");
 }
 
+static t_bool	parse_no(char *line, t_map *map)
+{
+	map->data.no_bool++;
+	if (map->data.no_bool > 1)
+		return (false);
+	return (get_textures(line + 2, NORTH, &map->data), true);
+}
+
+static t_bool	parse_so(char *line, t_map *map)
+{
+	map->data.so_bool++;
+	if (map->data.so_bool > 1)
+		return (false);
+	return (get_textures(line + 2, SOUTH, &map->data), true);
+}
+
 t_bool	parse_textures(char *line, t_map *map)
 {
 	if (line[0] == 'N' && line[1] == 'O')
-	{
-		map->data.no_bool++;
-		if (map->data.no_bool > 1)
-			return (false);
-		return (get_textures(line + 2, NORTH, &map->data), true);
-	}
+		return (parse_no(line, map));
 	else if (line[0] == 'S' && line[1] == 'O')
-	{
-		map->data.so_bool++;
-		if (map->data.so_bool > 1)
-			return (false);
-		return (get_textures(line + 2, SOUTH, &map->data), true);
-	}
+		return (parse_so(line, map));
 	else if (line[0] == 'E' && line[1] == 'A')
 	{
 		map->data.ea_bool++;
